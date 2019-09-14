2a50d3a20aa766f286f0a19262437617e3b32913
Alvin Ng <ngalvin93@gmail.com>
2019-09-13 23:44:50 -0500

    progress on wallpaper.js

--- wallpaper.js
+++ wallpaper.js
@@ -2,9 +2,33 @@ const wallpaper = require('wallpaper');
 const axios = require('axios');
 const download = require('download-file');
 
-(async () => {
-    await wallpaper.set('unicorn.jpg');
+var options = {
+    directory: "./img/",
+    filename: "dog.jpeg"
+}
 
-    await wallpaper.get();
-    //=> '/Users/sindresorhus/unicorn.jpg'
-})();
+// Gets the dog image
+
+axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(function (response) {
+        let url = response.data.message
+        return url
+    })
+    .then(function (response) {
+        console.log('This is the second then: ', response)
+        download(response, options)
+    })
+    .catch(function (error) {
+        console.error('There is an error! Here it is: ', error)
+    })
+
+// Downloads the dog image
+
+// Sets the dog image as wallpaper
+
+// (async () => {
+//     await wallpaper.set('unicorn.jpg');
+
+//     await wallpaper.get();
+//     //=> '/Users/sindresorhus/unicorn.jpg'
+// })();
